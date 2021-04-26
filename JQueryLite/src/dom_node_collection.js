9bab93bfacc2ec6e4765efe480e62505bf236dc9
Philip Lowe <philip.lowe94@gmail.com>
2021-04-26 16:42:52 -0400

    finished attr method

--- JQueryLite/src/dom_node_collection.js
+++ JQueryLite/src/dom_node_collection.js
@@ -5,6 +5,11 @@ class DOMNodeCollection{
 
     html(str) {
         //if it receives a str
+        if(str == "") {
+            for(let i = 0; i < this.htmlarr.length; i++){
+                this.htmlarr[i].innerHTML = "";
+            }
+        }
         if(!!str){
             //loop thru the htmlarr and make the innerHTML of each of them the str
             for(let i = 0; i < this.htmlarr.length; i++){
@@ -12,6 +17,40 @@ class DOMNodeCollection{
             }
         } else {
             return this.htmlarr[0].innerHTML;
+        }
+    }
+
+    empty() {
+        this.html("");
+    }
+
+    append(el){
+        if(el instanceof DOMNodeCollection){
+            let dnchtmls = el.htmlarr;
+            for(let i = 0; i < dnchtmls.length; i++) {
+                for(let y = 0; y < this.htmlarr.length; y++){
+                    this.htmlarr[y].innerHTML += dnchtmls[i].outerHTML;
+                }
+            }
+        } else if (el instanceof HTMLElement) {
+            for(let i = 0; y < this.htmlarr.length; i++){
+                this.htmlarr[i].innerHTML += el.outerHTML;
+            }
+        } else if (typeof el === 'string' || el instanceof String) {
+            for(let i = 0; i < this.htmlarr.length; i++){
+                
+                this.htmlarr[i].innerHTML += el;
+            }
+        }
+    }
+
+    attr(str, value) {
+        if(!value){
+            return this.htmlarr[0].getAttribute(str);
+        } else {
+            for(let i = 0; i < this.htmlarr.length; i++){
+                this.htmlarr[i].setAttribute(str, value);
+            }
         }
     }
 }
